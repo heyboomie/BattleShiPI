0b9aee94fd77ab780ec5113336dcf900e78ec041
heyboomie <109445551+heyboomie@users.noreply.github.com>
2026-05-22 13:09:36 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -173,6 +173,12 @@ def getTile(w):
         return(Letters.get(ptY // 61) + Num.get(ptX // 61))
     
 def blueprint(bow, dir, l, prev, w):
+    validLetters = {
+        "a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9
+        }
+    validNum = {
+        "0":0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9
+    }
     dirMap = [[0, 61], [61, 0], [0, -61], [-61, 0]]
     prev.undraW()
         topLeftX, topLeftY = 185, 85
