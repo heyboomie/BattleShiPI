7ca5653e5007a09a6b80fab40277fc7d8c023266
heyboomie <109445551+heyboomie@users.noreply.github.com>
2026-05-24 15:00:55 -0400

    Update networkConfig.py

--- networkConfig.py
+++ networkConfig.py
@@ -5,59 +5,54 @@ from random import randint
 class Server:
 
   def __init__(self):
-    try:
-        result = subprocess.run(["curl", "-s", "https://ifconfig.me"], capture_output=True, text=True, check=True)
-      #find the ip of the system on the network
-        localIP = result.stdout.strip()
-      #take the output and save it 
-    except subprocess.CalledProcessError as e:
-      #if there is an error with connecting, print it, then close the program
-        print( f"Error: {e}")
-        sys.end(0)
+    hostname = socket.gethostname()
+    self.localIP = socket.gethostbyname(hostname)
+    print(self.localIP)
     open = 1
     while open == 1:
       open = 1
-      port = randint(49152,65535)
+      self.port = randint(49152,65535)
       #dynamic ports
       #generate a random port 
       test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       test.settimeout(2)
-      open = test.connect_ex((localIP, port))
+      open = test.connect_ex((self.localIP, self.port))
       test.close
+      #print(self.port, open)
       #try and connect to that port and see if its open
       #connect_ex will return 0 if it connects
-    #once an ip and port are validated 
+      #once an ip and port are validated 
     try:
        
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((localIP, port))
-        s.listen(1)
-        cl, addr = s.accept()
-        print("Connected to ", addr)
-        return(cl)
+        s.bind((self.localIP, self.port))
+        s.listen()
+        self.cl, addr = s.accept()
+        print("Connected to", addr)
+
     except Exception as e:
-       print("An error with the connection has occured. Please check your connection then try again. Error code:" , e)
-       sys.end(0)
+       print("An error with the connection has occured. Please check your connection then try again: ", e)
+       sys.exit(0)
        
     #then open the server and make the conncetion 
   
-  def tmBoard(self, boatMap, cl):
+  def tmBoard(self, boatMap):
     #send the boat maps over
     packet = str(boatMap).encode()
     #turn the boatMap into sendable data
-    cl.sendall(packet)
+    self.cl.sendall(packet)
     #send that data to the client
-    mapString = (cl.revc(1024)).decode()
+    mapString = (self.cl.recv(1024)).decode()
     return(mapString)
 
-  def tmTurn(self, move:
+  def tmTurn(self, move):
     #send the shot
     packet = str(move).encode()
     #turn the shot location into sendable data
-    cl.sendall(packet)
+    self.cl.sendall(packet)
 
-  def rcTurn(self)
-    incoming = (cl.revc(1024)).decode()
+  def rcTurn(self):
+    incoming = (self.cl.recv(1024)).decode()
     return(incoming)
 
 class Client:
@@ -68,12 +63,11 @@ class Client:
     port = input("Please input the port of the Host: ")
     try:
         cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s, addr = cl.connect((ip, port))
+        self.s, addr = cl.connect((ip, port))
         print("Connected to ", addr)
-        return(s)
     except Exception as e:
-       print("An error with the connection has occured. Please check your connection then try again. Error code:" , e)
-       sys.end(0)
+       print("An error with the connection has occured. Please check your connection then try again:", e)
+       sys.exit(0)
     
     #then open the server and make the conncetion 
   
@@ -81,17 +75,20 @@ class Client:
     #send the boat maps over
     packet = str(boatMap).encode()
     #turn the boatMap into sendable data
-    s.sendall(packet)
+    self.s.sendall(packet)
     #send that data to the client
-    mapString = (s.revc(1024)).decode()
+    mapString = (self.s.recv(1024)).decode()
     return(mapString)
 
   def tmTurn(self, move):
     #send the shot
     packet = str(move).encode()
     #turn the shot location into sendable data
-    s.sendall(packet)
-    
-  def rcTurh(self):
-    incoming = (s.revc(1024)).decode()
+    self.s.sendall(packet)
+
+  def rcTurn(self):
+    incoming = (self.s.recv(1024)).decode()
     return(incoming)
+
+me = Server()
+print(vars(me))
