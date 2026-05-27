256c645ba89bd4a9dd68280003ae547d819e6bb8
heyboomie <109445551+heyboomie@users.noreply.github.com>
2026-05-27 14:38:30 -0400

    Create sfx.py

---
+++ sfx.py
@@ -0,0 +1,26 @@
+from playsound3 import playsound
+import os
+
+class sfx:
+
+  def __init__(self):
+    self.src = os.getcwd() + "/sfx/"
+
+  def hit(self):
+    playsound(self.src + "hit.wav", block=False)
+
+  def miss(self):
+    playsound(self.src + "miss.wav", block=False)
+
+  def sink(self):
+    playsound(self.src + "sink.wav", block=False)
+
+  def build(self):
+    playsound(self.src + "build.wav", block=False)
+
+  def win(self):
+    playsound(self.src + "win.wav", block=False)
+
+  def loss(self):
+    playsound(self.src + "loss.wav", block=False)
+    
