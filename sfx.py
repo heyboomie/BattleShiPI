4499eba36ca4a83745419f43428aa82f5285dac8
heyboomie <109445551+heyboomie@users.noreply.github.com>
2026-05-27 14:57:34 -0400

    Update sfx.py

--- sfx.py
+++ sfx.py
@@ -9,19 +9,29 @@ class sfx:
 
   def hit(self):
     playsound(self.src + "hit.wav", block=False)
+    #whistle then an explosion
 
   def miss(self):
     playsound(self.src + "miss.wav", block=False)
+    #whistle then a plop
 
   def sink(self):
     playsound(self.src + "sink.wav", block=False)
+    #blaring alarm
 
   def build(self):
     playsound(self.src + "build.wav", block=False)
+    #3 hammer hitting noises
 
   def win(self):
     playsound(self.src + "win.wav", block=False)
+    #trumpet fanfair
 
   def loss(self):
     playsound(self.src + "loss.wav", block=False)
+    #trumpet bwah bwah bwahhhh 
+
+  def ping(self):
+    playsound(self.src + "ping.wav", block=False)
+    #this one is the sound when you select server mode, or enter a host address, sonar ping
     
